695ea18381e4127539f533b04b0af64428aad016
Choi Eun Jeong <qhdrn67@naver.com>
2022-04-17 19:57:28 +0900

    [SCSS COMPILATION] Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import del from 'del';
 import ws from 'gulp-webserver';
 import gimage from 'gulp-image';
 
+const sass = require('gulp-sass')(require('node-sass'));
+
 const routes = {
   pug: {
     src: 'src/*.pug',
@@ -13,6 +15,11 @@ const routes = {
   img: {
     src: 'src/img/*',
     dest: 'build/img',
+  },
+  scss: {
+    src: 'src/scss/style.scss',
+    dest: 'build/css',
+    watch: 'src/scss/**/*.scss',
   },
 };
 
@@ -27,14 +34,21 @@ const webserver = () =>
 const img = () =>
   gulp.src(routes.img.src).pipe(gimage()).pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const live = gulp.parallel([webserver, watch]);
 
