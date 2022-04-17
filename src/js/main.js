ff95c4f547ea27d90ddb1a8288b8658e83ceacb3
Choi Eun Jeong <qhdrn67@naver.com>
2022-04-17 14:23:10 +0900

    Scaffolding Part Two

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import { random } from './util';
+
+const randomNum1 = random(10);
+const randomNum2 = random(20);
+
+console.log(randomNum1);
+console.log(randomNum2);
---
+++ src/js/util.js
@@ -0,0 +1,1 @@
+export const getRandom = (max) => Math.floor(Math.random() * max);
